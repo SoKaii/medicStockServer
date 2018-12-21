aba7325838827c2bc83ff99dc02c5e554d007973
SoKaii <t.martin92500@hotmail.fr>
2018-12-21 19:05:11 +0100

    Commentary
    Commentary of the server

--- medicStockServer/Server.cs
+++ medicStockServer/Server.cs
@@ -12,104 +12,95 @@ namespace medicStockServer
 {
     class Server
     {
-        List<List<List<string>>> medicList = new List<List<List<string>>>();
-        List<List<List<string>>> userList = new List<List<List<string>>>();
-        Int32 port = 0;
-        TcpListener ecoute = null;
-        int i = 0;
+        List<List<List<string>>> medicList = new List<List<List<string>>>(); // Création d'une liste qui stockera toutes les données médicaments récupérées de la dB
+        List<List<List<string>>> userList = new List<List<List<string>>>(); // Création d'une liste qui stockera toutes les données user récupérées de la dB
+
+        Int32 port = 0; // Création d'un entier qui servira comme port de connexion au Server
+        TcpListener ecoute = null; // Création d'une service d'écoute
+        int i = 0; // Création d'un indice permettant de splitter l'envoi par attribut 
 
         public Server(int p_port)
         {
-            DAO dao = new DAO();
-            medicList = dao.getMedicament();
-            userList = dao.getUser();
-
-            port = p_port;
             try
             {
-                // on crée le service - il écoutera sur toutes les interfaces réseau de la machine
-                ecoute = new TcpListener(IPAddress.Any, port);
-                // on le lance
-                ecoute.Start();
-                // boucle de service
-                TcpClient tcpClient = null;
-                // boucle infinie - sera arrêtée par Ctrl-C
-                while (true)
+                DAO dao = new DAO(); // Instanciation d'une dao 
+                medicList = dao.getMedicament(); // Récupération des médicaments
+                userList = dao.getUser(); // Récupératioin des utilisateurs
+
+                port = p_port; // Assignation du port de connexion
+            
+                ecoute = new TcpListener(IPAddress.Any, port); // Instanciation du service d'écoute 
+                ecoute.Start(); // Lancement du service d'écoute
+                TcpClient tcpClient = null; // Création d'un client virtuel afin de gérer le threading
+                while (true) // Boucle d'écoute
                 {
-                    // Attente d'un client
-                    Console.WriteLine("En attente d'un client");
-                    tcpClient = ecoute.AcceptTcpClient();
-                    // le service est assuré par une autre tâche
-                    ThreadPool.QueueUserWorkItem(Service, tcpClient);
+                    Console.WriteLine("En attente d'un client"); // Affichage d'un message d'attente
+                    tcpClient = ecoute.AcceptTcpClient(); // Acceptation du client 
+                    ThreadPool.QueueUserWorkItem(Service, tcpClient); // Création d'un thread pour le client précedemment accepté 
                 }
             }
-            catch (Exception ex)
+            catch (Exception ex) // Si l'éxécution rencontre un problème 
             {
-                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.Message); // Affichage du message d'érreur renvoyé 
             }
-            finally
+            finally // Lorsque le boucle d'écoute est arrêtée
             {
-                ecoute.Stop();
+                ecoute.Stop(); // Fermeture du service d'écoute
             }
         }
 
         public void Service(Object infos)
         {
-            String demande = null;
-            String reponse = null;
-            TcpClient client = infos as TcpClient;
-
+            String demande = null; // Création d'une string qui servira à stocker les demandes clients
+            TcpClient client = infos as TcpClient; // Création du client qui communiquera avec le serveur
 
             try
             {
-                Console.WriteLine("Client connected");
-                using (NetworkStream networkStream = client.GetStream())
+                Console.WriteLine("Client connected"); // Message confirmant la connexion du client au service 
+                using (NetworkStream networkStream = client.GetStream()) // Utilisant la laison entre le client et le serveur 
                 {
-                    using (StreamReader reader = new StreamReader(networkStream))
+                    using (StreamReader reader = new StreamReader(networkStream)) // Création d'un StreamReader servant à lire les demandes du client dans le Tcp
                     {
-                        using (StreamWriter writer = new StreamWriter(networkStream))
+                        using (StreamWriter writer = new StreamWriter(networkStream)) // Création d'un StreamWriter servant à écrire les réponses du serveur dans le Tcp 
                         {
-                            writer.AutoFlush = true;
+                            writer.AutoFlush = true; // Définit si le writer vide sa mémoire tampon entre chaque écriture 
                             
-                            while (demande != "exit")
+                            while (demande != "exit") // Tant que le client ne demande la fin du service
                             {
-                                demande = reader.ReadLine();
-                                writer.WriteLine(reponse);
-                                if (demande.ToLower().Equals("medic"))
+                                demande = reader.ReadLine(); // Demande prend la valeur de ce que le Reader lit dans le Tcp 
+                                if (demande.ToLower().Equals("medic")) // Si la demande du client est "medic"
                                 {
-                                    writer.WriteLine(reponse);
-
-                                    foreach (List<List<string>> subList in medicList)
+                                    foreach (List<List<string>> subList in medicList) // Pour chaque List dans medicList
                                     {
-                                        foreach (List<string> subSubList in subList)
+                                        foreach (List<string> subSubList in subList) // Pour chaque List qui est dans la List qui est dans medicList
                                         {
-                                            foreach (string data in subSubList)
+                                            foreach (string data in subSubList) // Pour chaque string qui est dans la List qui est dans la List qui est dans la medicList 
                                             {
-                                                writer.WriteLine(data);
-                                                i++;
-                                                if (i == 3)
+                                                writer.WriteLine(data); // Le server envoi au client la string précedemment ciblé
+                                                i++; // Incrémentation de i 
+                                                if (i == 3) // Si i vaut 3
                                                 {
-                                                    writer.WriteLine("\n");
-                                                    i = 0;
+                                                    writer.WriteLine("\n"); // Saut de ligne dans ce que renvoi le server 
+                                                    i = 0; // Remise à 0 de i 
                                                 }
                                             }
                                         }
                                     }
                                 }
-                                else if (demande.ToLower().Equals("user"))
+                                else if (demande.ToLower().Equals("user")) // Sinon si la demande du client concerne les utilisateurs 
                                 {
-                                    foreach (List<List<string>> subList in userList)
+                                    foreach (List<List<string>> subList in userList) // Pour chaque List dans userList
                                     {
-                                        foreach (List<string> subSubList in subList)
+                                        foreach (List<string> subSubList in subList) // Pour chaque List qui est dans la List qui est dans userList
                                         {
-                                            foreach (string data in subSubList)
+                                            foreach (string data in subSubList) // Pour chaque string qui est dans la List qui est dans la List qui est dans la userList 
                                             {
-                                                writer.WriteLine(data);
-                                                i++;
-                                                if (i == 3)
+                                                writer.WriteLine(data); // Le server envoi au client la string précedemment ciblé
+                                                i++; // Incrémentation de i 
+                                                if (i == 3) // Si i vaut 3
                                                 {
-                                                    writer.WriteLine("\n");
-                                                    i = 0;
+                                                    writer.WriteLine("\n"); // Saut de ligne dans ce que renvoi le server 
+                                                    i = 0; // Remise à 0 de i 
                                                 }
                                             }
                                         }
@@ -118,12 +109,11 @@ namespace medicStockServer
                             }
                         }
                     }
-
                 }
             }
-            catch (Exception e)
+            catch (Exception e) // Si une erreur est renvoyé dans l'éxécution du code
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine(e.Message); // Affichage du message d'erreur 
             }
             finally
             {}
